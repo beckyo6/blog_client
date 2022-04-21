55cbebff741050f9216c00642119d61f6521f1cf
Becky Ada <rebtshikadila@gmail.com>
2022-04-21 15:58:34 +0200

    ajout du contenu au footer

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,11 @@
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.0/umd/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script>
+    $('h6').mouseover(function() {
+        $(this).dropdown('toggle')
+    })
+</script>
+</body>
+
+</html>
