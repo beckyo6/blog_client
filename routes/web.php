b3a654d6f6d91dfee1e6caf34b3bb6442c2a4ca9
Becky Ada <rebtshikadila@gmail.com>
2022-04-21 16:23:26 +0200

    création de la route article.show

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,6 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('article/{id}/detail', [ArticleController::class, 'show'])->name('article.show');
 
