210009ec127cfc46c474475cc578bc34114604d7
Becky Ada <rebtshikadila@gmail.com>
2022-04-21 15:49:05 +0200

    ajout du contenu dans le header

---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Mon blog</title>
+
+    {{-- font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    {{-- end font --}}
+
+    {{-- css --}}
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    {{-- end css --}}
+
+    {{-- javascripts --}}
+    <script href="js/jquery-3.6.0.min.js"> </script>
+    <script href="js/bootstrap.min.js"> </script>
+    <script src="https://kit.fontawesome.com/14273d579a.js" crossorigin="anonymous"></script>
+    {{-- javascripts --}}
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
