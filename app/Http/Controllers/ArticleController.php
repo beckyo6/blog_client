312188f1370cefaec5ffb7385e43e2088a958ad7
Becky Ada <rebtshikadila@gmail.com>
2022-04-21 12:27:59 +0200

    implémentation de la methode index dans article controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -15,7 +15,19 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $articles = Article::select(
+            'articles.id',
+            'articles.titre',
+            'articles.image',
+            'articles.contenu',
+            'articles.created_at',
+            'categories.titre as categorie',
+            'users.name'
+        )->join('users', 'articles.user_id', 'users.id')
+        ->join('categories', 'articles.category_id', 'categories.id')
+        ->get();
+
+        return view('home', compact('articles'));
     }
 
     /**
