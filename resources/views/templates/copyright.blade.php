45bea76450953ce045df356c791032b4c032d443
Becky Ada <rebtshikadila@gmail.com>
2022-04-21 15:56:14 +0200

    ajout du contenu à la copyright

---
+++ resources/views/templates/copyright.blade.php
@@ -0,0 +1,5 @@
+<div class="main-footer d-flex p-2 px-3">
+    <span class="copyright ml-auto my-auto mr-2">Copyright © 2022, designed by
+        <a href="https://beckyada.com" rel="nofollow">Becky ada</a>
+    </span>
+</div>
