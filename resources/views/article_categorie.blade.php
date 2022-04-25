eef7f0257496133997165eeaa7033b8f5e29e2c9
Becky Ada <rebtshikadila@gmail.com>
2022-04-25 13:06:06 +0200

    affichage de la liste de 5 derniers articles par catégory

--- resources/views/article_categorie.blade.php
+++ resources/views/article_categorie.blade.php
@@ -3,29 +3,10 @@
     <div class="row">
         <div class="card-group">
             {{-- liste des articles --}}
-            <div class="col-md-7 offset-md-1 py-5">
+            <div class="col-md-6 offset-md-1 py-5">
                 <h1 id="post">Articles</h1>
                 <hr>
                 <div class="row py-1">
-                    {{-- <div class="col-md-12">
-                        <div class="card mb-3" style="height: 9.3em">
-                            <div class="row g-0">
-                                <div class="col-md-4">
-                                    <img src="img/ban-modified.jpeg" class="img-fluid rounded-start" alt="...">
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <a href="article/{{ $articledernier->id }}/detail">
-                                            <h5 class="card-title fw-bold">{{ $articledernier->titre }}</h5>
-                                        </a>
-                                        <p class="card-text text-start">
-                                            {{ Str::limit($articledernier->contenu, 150) }}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> --}}
                     @if ($articles->isNotEmpty())
                         @foreach ($articles as $article)
                             <div class="col-md-6">
@@ -54,7 +35,7 @@
                 <hr>
                 @if ($articlesderniers->isNotEmpty())
                     @foreach ($articlesderniers as $articlesdernier)
-                        <div class="col-md-6">
+                        <div class="col-md-6 ">
                             <div class="card text-left mb-3">
                                 <div class="card-body">
                                     <a href="article/{{ $articlesdernier->id }}/detail">
